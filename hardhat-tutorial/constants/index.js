0c5546b73ffd3b3a36f203dd360d0eae70a8d4fe
Nikomatt.eth <94803063+nikomatt69@users.noreply.github.com>
2022-02-22 16:21:34 +0100

    Add files via upload

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,10 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x05DdDe2a24fb7DC0f2f1C7B7604bF1896C8b658E";
-// URL to extract Metadata for a Crypto Dev NFT
-<<<<<<< HEAD
-const METADATA_URL = "https://nft-collection2-81i3liblc-nikomatt69.vercel.app/api/";
-=======
-const METADATA_URL = "https://nft-collection2-81i3liblc-nikomatt69.vercel.app/api/";
->>>>>>> d3468d1f508792f37ec4583cee309b7b9547bb07
+const WHITELIST_CONTRACT_ADDRESS = "0x98f5c62F7b5Bc735a64942dBec9456954c7a368A";
+// URL for the Crypto Devs NFT Metadata
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -5,6 +5,7 @@ const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
 async function main() {
   // Address of the whitelist contract that you deployed in the previous module
   const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+
   // URL from where we can extract the metadata for a Crypto Dev NFT
   const metadataURL = METADATA_URL;
   /*
