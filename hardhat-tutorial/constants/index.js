93b66bc6c1832c8c33120ca1a2cf3405515f3e41
Nikomatt.eth <94803063+nikomatt69@users.noreply.github.com>
2022-02-22 16:05:10 +0100

    Update index.js

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -2,9 +2,9 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x05DdDe2a24fb7DC0f2f1C7B7604bF1896C8b658E";
 // URL to extract Metadata for a Crypto Dev NFT
 <<<<<<< HEAD
-const METADATA_URL = "https://nft-collection2-two.vercel.app//api/";
+const METADATA_URL = "https://nft-collection2-81i3liblc-nikomatt69.vercel.app/api/";
 =======
-const METADATA_URL = "https://nft-collection2-two.vercel.app/api/";
+const METADATA_URL = "https://nft-collection2-81i3liblc-nikomatt69.vercel.app/api/";
 >>>>>>> d3468d1f508792f37ec4583cee309b7b9547bb07
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
